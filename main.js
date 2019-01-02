781fb6a4ae2506a3ebf2bcb73453b2ed1b8a7b13
ilovelili <route666@live.cn>
2019-01-02 19:46:57 +0900

    working version

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const casper = require('casper').create({
+    waitTimeout: 5000, // 5s
     verbose: true,
     logLevel: 'error',
     pageSettings: {
@@ -7,17 +8,19 @@ const casper = require('casper').create({
     }
 }),
     config = require('config.json'),
-    target_month = config['target_month'] || '2018-09',
+    target_month = config['target_month'] || formatMonth(),
+    preview_url_placeholder = config['preview_url_placeholder'],
     target_url_placeholder = config['target_url_placeholder'],
     url = config['url'],
     username = config['username'],
     password = config['password'],
-    target_url = target_url_placeholder.replace(new RegExp('{{time}}', 'g'), '2018-03-02'),
-    fs = require('fs');
+    fs = require('fs'),
+    previewlinks = resolvepreviewlinks();
+
+casper.userAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36');
 
 casper.start(url, function login() {
-    console.log('step 1: login');
-
+    this.echo('step 1: login');
     this.waitForSelector('form#login', function () {
         this.fill('form#login', {
             'userAccount': username,
@@ -27,21 +30,35 @@ casper.start(url, function login() {
 });
 
 casper.then(function open() {
-    this.echo(`step 2. open url ${target_url}`);
+    this.echo(`step 2. download`);
     this.waitForText('食安追溯', function () {
-        this.thenOpen(target_url, function () {
-            this.waitForSelector('a.btn1', function () {
-                this.capture('screenshots/export.png');
-                // 导出
-                this.click('.btn1');
+        // loop by date
+        this.each(previewlinks, function (self, preview_url) {
+            self.thenOpen(preview_url, function () {
+                if (self.exists('.product__item')) {
+                    let segments = preview_url.split('supplyDateMenu=');
+                    if (segments.length != 2) {
+                        return;
+                    }
+
+                    target_url = resolvedownloadlink(segments[1]);
+                    self.echo(`target url is ${target_url}`);
+                    self.thenOpen(target_url, function download() {
+                        self.waitForSelector('a.btn1', function () {
+                            // this.capture('screenshots/export.png');
+                            // 导出
+                            self.click('.btn1');
+                        });
+                    });
+                }
             });
-        })
+        });
     });
-})
+});
 
 casper.run();
 
-//Crawl------------------------
+// ------------------------ event handlers ------------------------
 casper.on('remote.message', function (msg) {
     this.echo('remote.msg: ' + msg);
 });
@@ -58,10 +75,11 @@ casper.on('run.complete', function () {
 // download
 casper.on('resource.received', function (resource) {
     if ((resource.url.indexOf('exportAllWares.htm?schoolIdstr=') !== -1)) {
-        const url = resource.url, file = '2018-03-02.xls';
+        const url = resource.url, file = resolvefilename(url);
         this.echo(`download url is ${url}`)
+        this.echo(`download file is ${file}`)
         try {
-            const download = fs.workingDirectory + '/' + file;
+            const download = fs.pathJoin(fs.workingDirectory, 'output', file);
             this.echo(`attempting to download file to ${download}`);
             this.download(url, download);
         } catch (e) {
@@ -69,3 +87,63 @@ casper.on('resource.received', function (resource) {
         }
     }
 });
+
+// ------------------------ helpers ------------------------
+function formatMonth(date) {
+    let d = new Date();
+    if (date instanceof Date) {
+        d = new Date(date);
+    }
+    let month = '' + (d.getMonth() + 1),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    return [year, month].join('-');
+}
+
+function formatDate(date) {
+    let d = new Date(date);
+    if (date instanceof Date) {
+        d = new Date(date);
+    }
+    let month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+function resolvepreviewlinks() {
+    let links = [];
+    const year = target_month.split('-')[0],
+        month = target_month.split('-')[1] - 1,
+        firstDay = new Date(year, month, 1),
+        lastDay = new Date(year, month + 1, 0),
+        today = new Date();
+
+    if (lastDay > today) {
+        lastDay = today
+    }
+
+    for (var d = firstDay; d <= lastDay; d.setDate(d.getDate() + 1)) {
+        preview_url = preview_url_placeholder.replace(new RegExp('{{time}}', 'g'), formatDate(d)),
+            links.push(preview_url);
+    }
+
+    return links;
+}
+
+function resolvedownloadlink(d) {
+    return target_url_placeholder.replace(new RegExp('{{time}}', 'g'), d);
+}
+
+function resolvefilename(target_url) {
+    const segments = target_url.split('supplyDateMenu=');
+    if (segments.length != 2) {
+        return '';
+    }
+    return segments[1] + '.xls';
+}
